6c511b3dcab34e2aa38dab9a47e67eccbbb5d9e6
Иван Смолин <ivan.smolin.78@mail.ru>
2023-03-22 02:24:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/layout/AdapterImage.java
@@ -0,0 +1,50 @@
+package com.example.layout;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class AdapterImage extends BaseAdapter {
+
+    private Context mContext;
+    List<MaskaImage> maskList;
+
+    public AdapterImage(Context mContext, List<MaskaImage> maskList) {
+        this.mContext = mContext;
+        this.maskList = maskList;
+    }
+
+    @Override
+    public int getCount() {
+        return maskList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return maskList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i)
+    {
+        return maskList.get(i).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = null;
+
+
+
+        return v;
+    }
+
+}
